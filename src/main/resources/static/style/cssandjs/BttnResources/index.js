4ff6f21b15957a3383031f3f56065e04897e54af
lkronlac <Lucas.Kronlachner@mic-cust.com>
2022-07-12 17:33:04 +0200

    Changes see sheet for design

---
+++ src/main/resources/static/style/cssandjs/BttnResources/index.js
@@ -0,0 +1,11 @@
+setTimeout(function(){
+    document.getElementById("noTime").className="";
+},500);
+
+function handlecreateClick(){
+    //Leeeeetttsseee gooooo ahahhahahahahahaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahhhhhhhhhhhhhhhh
+    const elem = document.getElementById("noTime")
+    elem.innerHTML = ""
+    elem.className = "ballAnim circle"
+    elem.id = ""
+}
